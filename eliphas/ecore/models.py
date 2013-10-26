3c034bba469d75820d189289686535baf9993bfc
Emil Dudev <emildudev@gmail.com>
2013-10-26 14:39:56 +0000

    Last commit for the working day

--- eliphas/ecore/models.py
+++ eliphas/ecore/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class Exam(models.Model):
 	name = models.CharField(max_length=64)
+	duration = models.IntegerField()
 
 	def TakeExam(self, user):
-		exam = user.exams.create(exam=self)
+		exam = user.exams.create(exam=self, starttime=timezone.now())
 		# todo: choose some random questions
 		for group in self.questiongroup_set.all():
 			for question in group.question_set.all():
-				exam.questioninstance_set.create(choice=None)
+				exam.questioninstance_set.create(question=question, choice=None)
 		return exam
 
 class QuestionGroup(models.Model):
-	exams = models.ManyToManyField(Exam)
+	exams = models.ManyToManyField(Exam, through='ExamQuestionGroupLink')
+
+class ExamQuestionGroupLink(models.Model):
+	exam = models.ForeignKey(Exam)
+	questiongroup = models.ForeignKey(QuestionGroup)
+	number = models.IntegerField()
 
 class Question(models.Model):
 	group = models.ForeignKey(QuestionGroup)
@@ -26,6 +33,7 @@ class Choice(models.Model):
 	text = models.CharField(max_length=512)
 
 class ExamInstance(models.Model):
+	starttime = models.DateTimeField()
 	exam = models.ForeignKey(Exam)
 	user = models.ForeignKey(User, related_name='exams')
 
@@ -33,6 +41,4 @@ class QuestionInstance(models.Model):
 	exam = models.ForeignKey(ExamInstance)
 	question = models.ForeignKey(Question)
 	choice = models.ForeignKey(Choice, null=True)
-
-
 
--- eliphas/ecore/views.py
+++ eliphas/ecore/views.py
@@ -18,14 +18,13 @@ def exam_detail(request, exam_id):
 
 def exam_questions(request, exam_id):
 	exam = get_object_or_404(Exam, pk=exam_id)
-	# todo: if user not assigned, create instace, else get instance
-	instance = request.user.exams.filter(exam_id=exam.id)
-	print instance
-	if not instance:
-		instance = exam.TakeExam(request.user)
+	examinstance = request.user.exams.filter(exam_id=exam_id)
+	if not examinstance:
+		examinstance = exam.TakeExam(request.user)
 	else:
-		instance = instance[0]
-	return render(request, 'ecore/exam_questions.html', {'exam': exam })
+		examinstance = examinstance[0]
+	import ipdb; ipdb.set_trace()
+	return render(request, 'ecore/exam_questions.html', {'examinstance': examinstance })
 
 
 from django.http import HttpResponseRedirect, HttpResponse
