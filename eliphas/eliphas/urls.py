26602ad1eafe55146181c773b97b29961f370fa8
Emil Dudev <emildudev@gmail.com>
2013-10-26 09:24:06 +0000

    Url fix

--- eliphas/eliphas/urls.py
+++ eliphas/eliphas/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'ecore.views.home', name='home'),
+#    url(r'^$', 'ecore.views.home', name='home'),
 #    url(r'^login/$', 'ecore.views.login'),
 #    url(r'^register/$', 'ecore.views.register'),
     #url(r'^eliphas/', include('eliphas.foo.urls')),
@@ -16,4 +16,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', include('ecore.urls')),
 )
