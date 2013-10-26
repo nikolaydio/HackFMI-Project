920cfe76b91e1eca8bec6e3cb1ce9c095cedb2d9
Emil Dudev <emildudev@gmail.com>
2013-10-26 10:36:01 +0000

    Exam detail

--- eliphas/ecore/urls.py
+++ eliphas/ecore/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, url
 from ecore import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home')
+	url(r'^$', views.home, name='home'),
+	url(r'^exam/(?P<exam_id>\d+)', views.exam_detail, name='exam_detail'),
+
 )
 
--- eliphas/ecore/views.py
+++ eliphas/ecore/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, render
+from ecore.models import Exam
 
 class menu_entry():
 	id = 1
@@ -12,6 +13,8 @@ def home(request):
 	menu = [menu_entry(1, "home"), menu_entry(2, "login")]
 	return render_to_response('ecore/home.html', {"menu_list": menu})
 
-def question(request):
-	pass
+def exam_detail(request, exam_id):
+	exam = get_object_or_404(Exam, pk=exam_id)
+	return render(request, 'ecore/exam_detail.html', {'exam': exam })
+
 
