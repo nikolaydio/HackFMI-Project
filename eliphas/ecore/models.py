07f69d307e408f4bee0dc732bdfe9ad8e8930797
Live session user <ubuntu@ubuntu.(none)>
2013-10-26 07:54:58 +0000

    First steps

--- eliphas/ecore/models.py
+++ eliphas/ecore/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Exam(models.Model):
+	name = models.CharField(max_length=64)
+	
+class Question(models.Model):
+	text = models.CharField(max_lenght=512)
+
+class Choice(models.Model):
+	question_id = models.ForeigKey(Question)
+	text = models.CharField(max_length=512)
+
+
