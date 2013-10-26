b645606af995c00a6a0f60c964047b6888f61b21
Nikolay Dionisov <nikolaydionisov@gmail.com>
2013-10-26 13:29:39 +0300

    Auth system

--- eliphas/ecore/urls.py
+++ eliphas/ecore/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 from ecore import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', 'ecore.views.login')
 )
 
--- eliphas/ecore/views.py
+++ eliphas/ecore/views.py
@@ -7,11 +7,36 @@ class menu_entry():
 	def __init__(self, id, name):
 		self.id = id
 		self.name = name
-
+from django.template import RequestContext
 def home(request):
 	menu = [menu_entry(1, "home"), menu_entry(2, "login")]
-	return render_to_response('ecore/home.html', {"menu_list": menu})
+	c = RequestContext(request, {"menu_list": menu})
+	return render_to_response('ecore/home.html', c)
 
-def question(request):
-	pass
+from django.http import HttpResponseRedirect, HttpResponse
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+import django.contrib.auth
 
+import django.forms as forms
+class AuthenticationForm(forms.Form):
+    username = forms.CharField(max_length=100)
+    password = forms.CharField(max_length=100)
+
+
+def login(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(username=username, password=password)
+		if user is not None:
+			if user.is_active:
+				django.contrib.auth.login(request, user)
+				return HttpResponseRedirect("/")
+			else:
+				return HttpResponse("not active")
+		else:
+			return HttpResponse("wrong username/pass")
+	else:
+		c = RequestContext(request, {'form':AuthenticationForm})
+		return render_to_response( "ecore/login.html", c)
